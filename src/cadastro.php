4fe22b8ba51df5fff23ece8c1b12d731bdc7eaea
WendellGSoares <wendell.soares@gmail.com>
2021-05-12 21:15:06 -0300

    Cadastro e login corrigidos

--- src/cadastro.php
+++ src/cadastro.php
@@ -9,36 +9,49 @@
     <title>Cadastro - Sanctus Grano</title>
 </head>
 <body>
+
     <div class="vh100">
         <div class="container-img">
             <img class="img" src="../img/eric-parks-TsNwtbCih0M-unsplash.jpg">
         </div>
-        <div class="conteudo">
-            <div class="container">
-                <nav class="menu">
-                    <ul class="menu">
-                        <li class="spacin"><a href="cafes.php">Produtos</a></li>
-                        <li class="spacin"><a href="sobre.html">Sobre</a></li>
-                    </ul>
-                    <ul>
-                        <li class="baixacess"><a href="index.html">Home</a></li>
-                    </ul>
-                </nav>
+    </div>
+
+    <div class="container2">
+
+            <div class="conteudo">
+                <div class="container">
+                    <nav class="menu">
+                        <ul class="submenu">
+                            <li class="spacin"><a href="cafes.php">Produtos</a></li>
+                            <li class="spacin"><a href="sobre.html">Sobre</a></li>
+                        </ul>
+                        <ul>
+                            <li class="baixacess"><a href="index.html">Home</a></li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
+
+        
+
+        <div class="bg-form">
+            
+                <form method="POST" action="cadastrar.php">
+                    <input name="nome" value="" placeholder="Nome Completo" class="campo" type="text" required="required">
+                    <input name="email" value="" placeholder="Email" class="campo" type="text" required="required">
+                    <input name="senha" value="" placeholder="Senha" class="campo" type="password" required="required">
+                    <input name="rsenha" value="" placeholder="Repita sua senha" class="campo" type="password" required="required">
+
+                    <div class="buttonf">
+                    <input class="btn_cc" type="button" onclick="redirectLogin();" value="Fazer login">
+                    <input class="btn_submit" type="submit" value="Criar conta">   
+                    </div>
+
+                </form>
+            
         </div>
     </div>
-    <div class="bg-form">
-        <div>
-            <form method="POST" action="cadastrar.php">
-                <input name="nome" value="" placeholder="Nome Completo" class="campo" type="text" required="required"><br><br><br>
-                <input name="email" value="" placeholder="Email" class="campo" type="text" required="required"><br><br><br>
-                <input name="senha" value="" placeholder="Senha" class="campo" type="password" required="required"><br><br><br>
-                <input name="rsenha" value="" placeholder="Repita sua senha" class="campo" type="password" required="required"><br><br><br><br>
-                <input class="btn_cc" type="button" onclick="redirectLogin();" value="Fazer login">
-                <input class="btn_submit" type="submit" value="Criar conta">
-            </form>
-        </div>
-    </div>
+    
 
     <script>
         function redirectLogin(){
